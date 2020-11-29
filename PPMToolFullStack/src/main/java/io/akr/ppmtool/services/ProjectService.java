6304fc52aa327b9a2643b24e96104cade9fb1054
Aravind KR <kraravind1993@gmail.com>
2020-11-29 19:05:33 +0530

    Services and Controller

---
+++ PPMToolFullStack/src/main/java/io/akr/ppmtool/services/ProjectService.java
@@ -0,0 +1,17 @@
+package io.akr.ppmtool.services;
+
+import io.akr.ppmtool.domain.Project;
+import io.akr.ppmtool.repository.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project){
+        return projectRepository.save(project);
+    }
+}
