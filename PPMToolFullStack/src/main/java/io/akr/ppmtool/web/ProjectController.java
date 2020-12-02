f2ff9d646bafe10dce3ff1203b2c3f53ae29dfe8
Aravind KR <kraravind1993@gmail.com>
2020-12-02 10:26:22 +0530

    Controller Validation

--- PPMToolFullStack/src/main/java/io/akr/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/io/akr/ppmtool/web/ProjectController.java
@@ -1,17 +1,22 @@
 package io.akr.ppmtool.web;
 
 import io.akr.ppmtool.domain.Project;
+import io.akr.ppmtool.services.MapValidationErrorService;
 import io.akr.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -20,11 +25,15 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
-        if(result.hasErrors()){
-            return new ResponseEntity<String>("Invalid Object", HttpStatus.BAD_REQUEST);
-        }
+
+        ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationService(result);
+        if(errorMap!=null) return errorMap;
+
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
     }
