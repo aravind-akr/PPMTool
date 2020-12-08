755578b0163880587427e77afc53612893663f56
Aravind KR <kraravind1993@gmail.com>
2020-12-08 10:21:19 +0530

    Find, delete, and list projects

--- PPMToolFullStack/src/main/java/io/akr/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/io/akr/ppmtool/services/ProjectService.java
@@ -21,4 +21,27 @@ public class ProjectService {
             throw new ProjectIDException("Project ID:'"+project.getProjectIdentifier().toUpperCase()+"' already exists");
         }
     }
+
+    public Project findByProjectIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(project == null){
+            throw new ProjectIDException("Project ID '"+projectId+"' doesn't exist");
+        }
+        return project;
+    }
+
+    public Iterable<Project> findAllProjects(){
+        return projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId);
+
+        if(project == null) {
+            throw new ProjectIDException("Cannot delete project with ID'"+projectId+"'. This project doesn't exist");
+        }
+
+        projectRepository.delete(project);
+    }
 }
--- PPMToolFullStack/src/main/java/io/akr/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/io/akr/ppmtool/web/ProjectController.java
@@ -8,15 +8,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 @RestController
 @RequestMapping("/api/project")
@@ -36,6 +37,25 @@ public class ProjectController {
 
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
+    }
+    
+    @GetMapping("/{projectId}")
+    public  ResponseEntity<?> getProjectById(@PathVariable String projectId){
+
+        Project project = projectService.findByProjectIdentifier(projectId);
+
+        return new ResponseEntity<Project>(project,HttpStatus.OK);
+    }
+    
+    @GetMapping("/all")
+    public Iterable<Project> getAllProjects(){
+        return projectService.findAllProjects();
+    }
+
+    @DeleteMapping("/{projectId}")
+    public ResponseEntity<?> deleteProject(@PathVariable String projectId){
+        projectService.deleteProjectByIdentifier(projectId.toUpperCase());
+        return new ResponseEntity<String>("Project with ID '"+projectId+"'was deleted,",HttpStatus.OK);
     }
 
 }
